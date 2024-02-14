6e53a1ebba473c9717b684be9ada3b8017194ed0
Philipp Wolfger <philipp.wolfger@alturos.com>
2024-02-14 23:54:23 +0100

    Removed some things

--- twep-frontend/src/app/account/account.component.ts
+++ twep-frontend/src/app/account/account.component.ts
@@ -1,4 +1,4 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule} from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { FormControl, Validators } from '@angular/forms';
@@ -8,34 +8,22 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
 
-import { BikeStationService } from '../service/bikeStation.service';
-import { BikeStation } from '../model/bikeStation';
-import { Location } from '../model/location';
-
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatDividerModule } from '@angular/material/divider';
-import { MatSelectModule, MatSelectChange } from '@angular/material/select';
+import { MatSelectModule } from '@angular/material/select';
 
 import { WalletService } from '../service/wallet.service';
 import { OnInit } from '@angular/core';
-import { TicketService } from '../service/tickets.service'; 
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
 
 import {AuthService} from '../service/auth.service';
-
 
 import { AccountService } from '../service/account.service';
 import { UserAccount } from '../model/user-account';
 
 import { ViewChild, ElementRef } from '@angular/core';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
-
-import { Pipe, PipeTransform } from '@angular/core';
 
 @Component({
   selector: 'app-account',
@@ -72,9 +60,6 @@ export class AccountComponent implements OnInit {
   loggedInUserId: string | null = null;
   userId: string | null = null;
   walletAmount: number | null = null;
-
-
-
   saveSuccessMessage: string | null = null;
 
 
@@ -82,10 +67,11 @@ export class AccountComponent implements OnInit {
   constructor(
     private accountService: AccountService,
     private authService: AuthService,
-    private sanitizer: DomSanitizer,
     private walletService: WalletService,
     
-    ) { this.getUserId();}
+    ) { 
+      this.getUserId()
+    }
 
     getUserId(): void {
       this.userId = this.authService.getLoggedInUserId(); 
@@ -97,47 +83,36 @@ export class AccountComponent implements OnInit {
     this.loadUserAccount();
   }
 
-  
-
   loadUserAccount() {
-   
     if (this.loggedInUserId) {
-      this.accountService.getUserAccount(this.loggedInUserId).subscribe(
-        (data: UserAccount) => {
-          this.userAccount = data;
-        },
-        (error) => {
-          console.error('Error fetching user account:', error);
+        this.accountService.getUserAccount(this.loggedInUserId).subscribe({
+          next: (data: UserAccount) => {
+            this.userAccount = data;
+          }
         }
       );
     }
   }
 
-
-
- 
-
-
-
 saveAdding(): void {
-    const amountToAdd: number = parseFloat(this.addMoneyFormControl.value!);
+    const amountToAdd: number = Number(this.addMoneyFormControl.value);
 
-    if (!isNaN(amountToAdd)) {
-        this.walletService.addMoneyToWallet(this.userId!, amountToAdd)
-            .subscribe({
-                next: response => {
-                    console.log('Money got added to wallet: ', response);
-                    this.addMoneyFormControl.reset();
-                    this.loadUserAccount();
-                    this.saveSuccessMessage = "Money successfully added to wallet.";
-                    setTimeout(() => {
-                        this.saveSuccessMessage = null; 
-                    }, 2000); 
-                },
-                error: error => {
-                    console.error('Error while adding money', error);
-                }
-            });
+    if (!Number.isNaN(amountToAdd)) {
+      this.walletService.addMoneyToWallet(this.userId!, amountToAdd)
+          .subscribe({
+              next: response => {
+                  console.log('Money got added to wallet: ', response);
+                  this.addMoneyFormControl.reset();
+                  this.loadUserAccount();
+                  this.saveSuccessMessage = "Money successfully added to wallet.";
+                  setTimeout(() => {
+                      this.saveSuccessMessage = null; 
+                  }, 2000); 
+              },
+              error: error => {
+                  console.error('Error while adding money', error);
+              }
+          });
     }
 }
 }
